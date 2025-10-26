aad9c145db846f15bb6b0d4f0ca1e74be04decc4
Neurophile-pro <lakshay_m@mfs.iitr.ac.in>
2025-10-26 05:56:58 +0530

    deployment fixes

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/context_AuthContext';
 import ProtectedRoute from "./components/Layout/components_Layout_ProtectedRoute";
